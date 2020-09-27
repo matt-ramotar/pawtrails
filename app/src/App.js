b906385f627c4127ca2fb7dcac290d94cce8a405
Matt Ramotar <matt.ramotar@jhu.edu>
2020-09-26 17:49:38 -0700

    Set up material-ui

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MultipleSelect from './components/MultipleSelect';
+import MenuAppBar from './components/AppBar';
 
 function App() {
+  const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MenuAppBar></MenuAppBar>
+        <MultipleSelect></MultipleSelect>
+        <BrowserRouter>
+          <Route exact path='/'>
+            <h1>Paw Trails</h1>
+            <Button
+              variant='contained'
+              color='primary'
+              className={classes.button}>
+              Material-Ui
+            </Button>
+          </Route>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
