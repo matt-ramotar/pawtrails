eec1d3d947201b68ce277900399d6caa214941ac
Matt Ramotar <matt.ramotar@jhu.edu>
2020-09-28 19:10:29 -0700

    Add Tag seeds

---
+++ backend/db/scrapers/genTags.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+
+const { trails } = require('../data/trails');
+const db = require('../models');
+const { Tag } = db;
+
+(async () => {
+  const uniqueTags = new Set();
+  for (const trail of trails) {
+    // City
+    trail.tags.forEach(tag => uniqueTags.add(tag));
+  }
+
+  const tags = Array.from(uniqueTags);
+
+  for (let tag of tags) {
+    await fs.appendFile(
+      '../data/json/tags.json',
+      JSON.stringify({ tag, createdAt: 'new Date()', updatedAt: 'new Date()' })
+    );
+  }
+  // await Tag.create({ tag });
+})();
