1215bebd014e7fba247e32d0215fc35364c3a6d4
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-01 09:57:30 -0700

    Connect review Reducer

--- app/src/store/configureStore.js
+++ app/src/store/configureStore.js
@@ -2,10 +2,12 @@ import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import auth from './auth';
 import trails from './trails';
+import reviews from './reviews';
 
 const rootReducer = combineReducers({
   auth,
   trails,
+  reviews,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
