f240747925733dffabf78844cdea6f88c5eb9fa3
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-02 10:40:32 -0700

    Add Redux action to save to list

--- app/src/store/lists.js
+++ app/src/store/lists.js
@@ -1,10 +1,20 @@
 const CREATE_LIST = 'lists/CREATE_LIST';
+const SAVE_TO_LIST = 'lists/SAVE_TO_LIST';
 
-export const createList = name => async dispatch => {
+export const createList = ({ userId, listName }) => async dispatch => {
   const response = await fetch('/api/lists/new', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(name),
+    body: JSON.stringify({ userId, listName }),
+  });
+  console.log(response);
+};
+
+export const saveToList = ({ userId, trailId, listName }) => async dispatch => {
+  const response = await fetch(`/api/lists/${listName}`, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ userId, trailId }),
   });
   console.log(response);
 };
