ebad8772c7e498bdae9925446ea1bdcbfce165ce
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-02 10:37:28 -0700

    Add route to create list

--- backend/routes/api/lists.js
+++ backend/routes/api/lists.js
@@ -6,6 +6,20 @@ const { List, UserTrail, User, Trail } = require('../../db/models');
 // lists/favorites
 // get favorite list
 // add trail to list
+
+router.post(
+  '/new',
+  asyncHandler(async (req, res, next) => {
+    const { userId, listName } = req.body;
+
+    // get userTrail where userId trailId and listId name attribute equals 'My favorites'
+    const list = await List.create({ name: listName });
+
+    const userTrail = await UserTrail.create({ userId, listId: list.id });
+    return res.json(userTrail);
+  })
+);
+
 router.post(
   '/favorites',
   asyncHandler(async (req, res, next) => {
@@ -31,5 +45,6 @@ router.post(
     ).Lists[0].id;
 
     const newFavoriteTrail = await UserTrail.create({ trailId, userId, listId: userFavTrailsId });
+    return res.json(userTrail);
   })
 );
