e7fa1cc7c1508e24dfa0fcf72d17c313281d0edf
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-08 19:40:53 -0700

    Style

--- app/src/components/AppBar.js
+++ app/src/components/AppBar.js
@@ -11,6 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import LogoImg from '../vector60-3765-01.jpg';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,7 +25,7 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
     color: '#414141',
-    fontSize: '14px',
+    fontSize: '16px',
   },
 }));
 
@@ -33,18 +34,29 @@ export default function ButtonAppBar() {
   const loggedOut = useSelector(state => !state.auth.jti);
   return (
     <div className={classes.root}>
-      <AppBar position='static' className={classes.root} display='flex' flexDirection='row'>
+      <AppBar position='static' className={classes.root}>
         <Toolbar>
-          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
-            <MenuIcon />
-          </IconButton>
-          <Link href='/'>
-            <Typography variant='h6' className={classes.title}>
-              Paw Trails
-            </Typography>
-          </Link>
-          <Box display='flex' flexDirection='row'>
-            {!loggedOut ? <LogoutButtonContainer /> : <LoginSignupContainer />}
+          <Box style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Box>
+              <IconButton
+                edge='start'
+                className={classes.menuButton}
+                color='inherit'
+                aria-label='menu'>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+
+            <Box>
+              <Link href='/' style={{ textDecoration: 'none' }}>
+                <img src={LogoImg} height='36px' width='36px' style={{ 'margin-bottom': '-8px' }} />
+                <span className={classes.title}>Paw Trails</span>
+              </Link>
+            </Box>
+
+            <Box display='flex' flexDirection='row'>
+              {!loggedOut ? <LogoutButtonContainer /> : <LoginSignupContainer />}
+            </Box>
           </Box>
         </Toolbar>
       </AppBar>
