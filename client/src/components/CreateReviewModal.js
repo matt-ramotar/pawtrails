3881f82f281a337445e92e3d00a34b7779c1981a
Matt Ramotar <matt.ramotar@jhu.edu>
2020-11-20 15:07:15 -0800

    Clean

--- client/src/components/CreateReviewModal.js
+++ client/src/components/CreateReviewModal.js
@@ -1,11 +1,4 @@
-import React, { useState, useCallback } from 'react';
-import { connect } from 'react-redux';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import TextField from '@material-ui/core/TextField';
+import React from 'react';
 
 export default function CreateReviewModal() {
   return <h1>Review Modal</h1>;
