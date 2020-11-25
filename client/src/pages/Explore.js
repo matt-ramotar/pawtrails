5e2c357e678b035b1aba85e0f6a46f1838c5a468
Matt Ramotar <matt.ramotar@jhu.edu>
2020-11-24 16:54:26 -0800

    Add Explore page

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { sidebarWidth } from '../components/Sidebar';
+import GoogleMapReact from 'google-map-react';
 
 export default function Explore() {
+  const zoom = 14;
+  const center = { lat: 45.765, lng: -123.9777161 };
+  const defaultOptions = {
+    fullscreenControl: false,
+  };
+
   return (
-    <div>
-      <h1>Map</h1>
+    <div style={{ width: `calc(100% - ${sidebarWidth}px)`, height: '100vh', marginLeft: sidebarWidth }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyA4fFeyvSm7WKc6-V-HSWa1h_aNxu-Zico' }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+        defaultOptions={defaultOptions}></GoogleMapReact>
     </div>
   );
 }
