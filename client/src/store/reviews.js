6f7f0616754ddeb3e9757f1b1ff92a604f715123
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-04 14:10:22 -0500

    Clean

--- client/src/store/reviews.js
+++ client/src/store/reviews.js
@@ -1,18 +1,11 @@
 const CREATE_REVIEW = 'reviews/CREATE_REVIEW';
 
 export const createReview = data => async dispatch => {
-  console.log(data);
-  const response = await fetch('/api/reviews', {
+  const res = await fetch('/api/reviews', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
-
-  console.log(response);
-  // if (response.ok) {
-  //   // dispatch(hideForm());
-  //   // dispatch(getPokemon());
-  // }
 };
 
 export default function reviewsReducer(state = {}, action) {
