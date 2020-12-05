b9ec5839d5b37bb9000a9b9f6f3aa3b14b4d4c3c
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-05 14:55:23 -0500

    Update frontend reviews Redux store

--- client/src/store/reviews.js
+++ client/src/store/reviews.js
@@ -1,4 +1,5 @@
 const CREATE_REVIEW = 'reviews/CREATE_REVIEW';
+const SET_TRAIL_REVIEWS = 'reviews/SET_TRAIL_REVIEWS';
 
 export const createReview = data => async dispatch => {
   const res = await fetch('/api/reviews', {
@@ -8,10 +9,22 @@ export const createReview = data => async dispatch => {
   });
 };
 
+export const setTrailReviews = reviews => ({ type: SET_TRAIL_REVIEWS, reviews });
+
+export const loadTrailReviews = trailId => async dispatch => {
+  const res = await fetch(`/api/trails/${trailId}/reviews`);
+  if (res.ok) {
+    const reviews = await res.json();
+    dispatch(setTrailReviews(reviews));
+  }
+};
+
 export default function reviewsReducer(state = {}, action) {
   switch (action.type) {
     case CREATE_REVIEW:
       return { ...state, current: action.current };
+    case SET_TRAIL_REVIEWS:
+      return { ...state, trailReviews: action.reviews };
     default:
       return state;
   }
