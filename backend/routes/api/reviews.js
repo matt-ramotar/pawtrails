fc9f4a797b74006f8d8345d6a1f9ac62da112186
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-05 13:19:09 -0500

    Update summary statistics on review submission

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,7 +1,16 @@
 const asyncHandler = require('express-async-handler');
 const express = require('express');
 
-const { Trail, Photo, Tag, City, Review, ReviewTrailCondition, ReviewReaction } = require('../../db/models');
+const {
+  Trail,
+  Photo,
+  Tag,
+  City,
+  Review,
+  ReviewTrailCondition,
+  ReviewReaction,
+  TrailSummary,
+} = require('../../db/models');
 
 const router = express.Router();
 
@@ -18,17 +27,47 @@ router.post(
       date,
     });
 
+    const trail = await Trail.findByPk(trailId, {
+      include: { all: true, nested: true },
+    });
+    const trailSummaryId = trail.TrailSummary.id;
+
+    const trailSummary = await TrailSummary.findByPk(trailSummaryId);
+
+    const { summary } = trailSummary;
+    console.log(summary);
+
+    summary['counts']['stars']['_total']++;
+    summary['counts']['stars'][rating]++;
+    summary['counts']['reviews']['_total']++;
+
     const trailConditionsToAdd = Object.keys(trailConditions);
 
     for (const trailCondition of trailConditionsToAdd) {
       await ReviewTrailCondition.create({ reviewId: review.id, trailCondition });
+
+      summary['counts']['trailConditions'][trailCondition] = summary['counts']['trailConditions'][trailCondition]
+        ? summary['counts']['trailConditions'][trailCondition] + 1
+        : 1;
+
+      console.log(summary);
     }
 
     const reactionsToAdd = Object.keys(reactions);
 
     for (const reaction of reactionsToAdd) {
       await ReviewReaction.create({ reviewId: review.id, reaction });
+
+      summary['counts']['reactions'][reaction] = summary['counts']['reactions'][reaction]
+        ? summary['counts']['reactions'][reaction] + 1
+        : 1;
+
+      console.log(summary);
     }
+
+    trailSummary.summary = summary;
+    trailSummary.changed('summary', true);
+    await trailSummary.save();
 
     return res.json(true);
   })
