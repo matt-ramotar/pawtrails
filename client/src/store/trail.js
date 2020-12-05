169461b87dc1fad7864cccec2a8c595c72ca3b32
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-05 14:55:31 -0500

    Update frontend trail Redux store

--- client/src/store/trail.js
+++ client/src/store/trail.js
@@ -2,6 +2,14 @@ const SET_TRAIL = 'trail/SET_TRAIL';
 
 export const setTrail = trail => ({ type: SET_TRAIL, trail });
 
+export const loadTrail = trailId => async dispatch => {
+  const res = await fetch(`/api/trails/${trailId}`);
+  if (res.ok) {
+    const trail = await res.json();
+    dispatch(setTrail(trail));
+  }
+};
+
 export default function trailReducer(state = {}, action) {
   switch (action.type) {
     case SET_TRAIL:
