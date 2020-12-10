0f0dfc4ec9bba2884d3a5f0a1cbdd06fbd0f71e6
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-10 17:13:24 -0500

    Fix route name

--- backend/routes/api/lists.js
+++ backend/routes/api/lists.js
@@ -1,22 +1,21 @@
 const asyncHandler = require('express-async-handler');
 const router = require('express').Router();
 
-const { List, UserTrail, User, Trail } = require('../../db/models');
+const { List, User, TrailList } = require('../../db/models');
 
 // lists/favorites
 // get favorite list
 // add trail to list
 
 router.post(
-  '/new',
+  '/',
   asyncHandler(async (req, res, next) => {
     const { userId, listName } = req.body;
 
     // get userTrail where userId trailId and listId name attribute equals 'My favorites'
-    const list = await List.create({ name: listName });
+    const list = await List.create({ name: listName, userId });
 
-    const userTrail = await UserTrail.create({ userId, listId: list.id });
-    return res.json(userTrail);
+    return res.json(list);
   })
 );
 
@@ -46,7 +45,7 @@ router.post(
 
       console.log(userFavTrailsId);
 
-      const newFavoriteTrail = await UserTrail.create({ trailId, userId, listId: userFavTrailsId });
+      const newFavoriteTrail = await TrailList.create({ trailId, listId: userFavTrailsId });
       return res.json(newFavoriteTrail);
     } catch (e) {
       console.log(e);
--- client/src/store/lists.js
+++ client/src/store/lists.js
@@ -2,7 +2,7 @@ const CREATE_LIST = 'lists/CREATE_LIST';
 const SAVE_TO_LIST = 'lists/SAVE_TO_LIST';
 
 export const createList = ({ userId, listName }) => async dispatch => {
-  const response = await fetch('/api/lists/new', {
+  const response = await fetch('/api/lists', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ userId, listName }),
