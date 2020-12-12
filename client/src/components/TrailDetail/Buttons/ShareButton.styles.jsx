d9dfa170548c3021df1eab5ec64187e99ca821bf
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-12 12:54:20 -0500

    Style share button

--- client/src/components/TrailDetail/Buttons/ShareButton.styles.jsx
+++ client/src/components/TrailDetail/Buttons/ShareButton.styles.jsx
@@ -16,43 +16,25 @@ export const useStyles = makeStyles(theme => ({
     borderRadius: '50%',
   },
 
-  button__disabled: {
-    backgroundColor: '#414141',
-    minWidth: 40,
-    minHeight: 40,
-    maxHeight: 40,
-    maxWidth: 40,
-    borderRadius: '50%',
-  },
-
   label: {
     color: '#1D72E7',
     fontFamily: 'Roboto',
     fontWeight: 'bold',
   },
 
-  label__disabled: {
-    color: '#414141',
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
-  },
-
   popover: {
     backgroundColor: 'transparent',
-    boxShadow: 'none',
     overflow: 'hidden',
     marginTop: -60,
     marginLeft: -20,
   },
 
-  iconsBox: {
+  icons__box: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     width: 150,
     backgroundColor: '#414141',
-    opacity: 0.8,
-    borderRadius: 10,
   },
 }));
